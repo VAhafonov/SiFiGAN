c305f2e637708008b7606580a92c007d19d54e45
Volodymyr Ahafonov <agafonov.vladymyrovich@gmail.com>
2023-07-22 14:30:09 +0200

    Fix issue in decode.py

--- sifigan/nv_triton/client/decode.py
+++ sifigan/nv_triton/client/decode.py
@@ -190,7 +190,7 @@ def decode_main(input_dir: str, output_dir: str, path_to_config: str) -> None:
                 # print('c shape', c.shape)
                 # print('dfs shape', dfs.shape)
                 # print('true_lengths shape', true_lengths.shape)
-                y = results
+                y = results.as_numpy('OUTPUT__0')
                 # print("!!!!!", torch.isnan(y).any())
                 rtf = (time() - start) / (y.size(-1) / config.data.sample_rate)
                 pbar.set_postfix({"RTF": rtf})
