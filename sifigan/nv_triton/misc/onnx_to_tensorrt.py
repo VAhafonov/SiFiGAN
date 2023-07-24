592c84a023a29d478962b3863a35a91fa3c18949
Volodymyr Ahafonov <agafonov.vladymyrovich@gmail.com>
2023-07-24 21:02:01 +0200

    Update shapes in onnx_to_tensorrt script

--- sifigan/nv_triton/misc/onnx_to_tensorrt.py
+++ sifigan/nv_triton/misc/onnx_to_tensorrt.py
@@ -22,9 +22,9 @@ def onnx_to_tensorrt_main(onnx_model_path: str, trt_model_pah: str, fp16: bool):
 
     # Create an optimization profile for dynamic input shapes
     profile = builder.create_optimization_profile()
-    profile.set_shape("INPUT__0", (1, 1, 12000), (1, 1, 75240), (1, 1, 75240))
-    profile.set_shape("INPUT__1", (1, 43, 100), (1, 43, 627), (1, 43, 627))
-    profile.set_shape("INPUT__2", (1, 4, 12000), (1, 4, 75240), (1, 4, 75240))
+    profile.set_shape("INPUT__0", (1, 1, 24000), (1, 1, 75240), (1, 1, 75240))
+    profile.set_shape("INPUT__1", (1, 43, 200), (1, 43, 627), (1, 43, 627))
+    profile.set_shape("INPUT__2", (1, 4, 24000), (1, 4, 75240), (1, 4, 75240))
     config.add_optimization_profile(profile)
     print(config)
 
