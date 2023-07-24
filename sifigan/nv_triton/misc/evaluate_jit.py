bf62d118648ce8d1942386a6e33d8a6adcf83e8b
Volodymyr Ahafonov <agafonov.vladymyrovich@gmail.com>
2023-07-24 15:36:41 +0200

    Add tensors equality in evaluate_jit.py

--- sifigan/nv_triton/misc/evaluate_jit.py
+++ sifigan/nv_triton/misc/evaluate_jit.py
@@ -1,5 +1,6 @@
 import argparse
 
+import numpy as np
 import torch.jit
 
 from sifigan.nv_triton.misc.utils_funcs import read_and_preprocess_test_tensors
@@ -10,7 +11,13 @@ def evaluate_jit_main(jit_model_path: str, test_tensor_path: str):
     input_data, output_data = read_and_preprocess_test_tensors(test_tensor_path, do_read_output_tensor=True,
                                                                do_convert_to_cuda=False)
     jit_output = jit_model(input_data.in_signal, input_data.c, input_data.dfs, input_data.true_length)
-    print(jit_output)
+
+    # compare tensors
+    jit_output_np = jit_output.numpy()
+    target_output = output_data.y.numpy()
+    are_tensors_equal = np.allclose(jit_output_np[~np.isnan(jit_output_np)],
+                                    target_output[~np.isnan(target_output)])
+    print(are_tensors_equal)
     pass
 
 
