67775109aa5f24f7729a49fd21b0deabe4611bd0
Volodymyr Ahafonov <agafonov.vladymyrovich@gmail.com>
2023-07-24 15:33:32 +0200

    Fix issue in evaluate_jit script

--- sifigan/nv_triton/misc/evaluate_jit.py
+++ sifigan/nv_triton/misc/evaluate_jit.py
@@ -8,7 +8,7 @@ from sifigan.nv_triton.misc.utils_funcs import read_and_preprocess_test_tensors
 def evaluate_jit_main(jit_model_path: str, test_tensor_path: str):
     jit_model = torch.jit.load(jit_model_path)
     input_data, output_data = read_and_preprocess_test_tensors(test_tensor_path, do_read_output_tensor=True,
-                                                               do_convert_to_cuda=True)
+                                                               do_convert_to_cuda=False)
     jit_output = jit_model(input_data.in_signal, input_data.c, input_data.dfs, input_data.true_length)
     print(jit_output)
     pass
