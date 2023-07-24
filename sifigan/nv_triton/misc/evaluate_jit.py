e6d01067fbf2a24d53e1615e99044bd938cbaaee
Volodymyr Ahafonov <agafonov.vladymyrovich@gmail.com>
2023-07-24 16:34:52 +0200

    Cleanup evaluate_jit script

--- sifigan/nv_triton/misc/evaluate_jit.py
+++ sifigan/nv_triton/misc/evaluate_jit.py
@@ -7,9 +7,12 @@ from sifigan.nv_triton.misc.utils_funcs import read_and_preprocess_test_tensors
 
 
 def evaluate_jit_main(jit_model_path: str, test_tensor_path: str):
+    # load model
     jit_model = torch.jit.load(jit_model_path)
+    # load test data
     input_data, output_data = read_and_preprocess_test_tensors(test_tensor_path, do_read_output_tensor=True,
                                                                do_convert_to_cuda=False)
+    # predict
     jit_output = jit_model(input_data.in_signal, input_data.c, input_data.dfs, input_data.true_length)
 
     # compare tensors
@@ -19,7 +22,6 @@ def evaluate_jit_main(jit_model_path: str, test_tensor_path: str):
     print("Tensors are equal:", are_tensors_equal)
     print("Max diff in tensors:",
           np.max(np.abs(jit_output_np[~np.isnan(jit_output_np)] - target_output[~np.isnan(target_output)])))
-    pass
 
 
 if __name__ == '__main__':
