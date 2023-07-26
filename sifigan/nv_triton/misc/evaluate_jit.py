5c8e02f208a9006b28d07c6db55fc27e34c40733
Volodymyr Ahafonov <agafonov.vladymyrovich@gmail.com>
2023-07-26 15:24:30 +0200

    Add more debug info in evaluate_jit.py

--- sifigan/nv_triton/misc/evaluate_jit.py
+++ sifigan/nv_triton/misc/evaluate_jit.py
@@ -33,10 +33,13 @@ def evaluate_jit_main(jit_model_path: str, test_tensor_path: str, fp16: bool = F
     are_tensors_equal = np.allclose(jit_output_np, target_output, equal_nan=True, atol=1e-5)
     print("Tensors are equal:", are_tensors_equal)
     diff = np.abs(jit_output_np - target_output)
+    diff_rel = np.divide(diff, target_output)
     print("Max diff in tensors:", np.max(diff))
     print("Mean diff in tensors:", np.mean(diff))
-    print("Man signal in target tensor:", np.mean(target_output))
-    print("Man signal in predicted tensor:", np.mean(jit_output_np))
+    print("Mean signal in target tensor:", np.mean(target_output))
+    print("Mean signal in predicted tensor:", np.mean(jit_output_np))
+    print("Max rel diff in tensors:", np.max(diff_rel))
+    print("Mean rel diff in tensors:", np.mean(diff_rel))
 
 
 if __name__ == '__main__':
