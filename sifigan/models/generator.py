78b91892dcbaef259eecf916277c6466d9199001
Volodymyr Ahafonov <agafonov.vladymyrovich@gmail.com>
2023-07-26 15:20:23 +0200

    Try to move self.fn["output_conv"] to half precision

--- sifigan/models/generator.py
+++ sifigan/models/generator.py
@@ -505,7 +505,7 @@ class SiFiGANGenerator(nn.Module):
         #     for j, fn_block in enumerate(module_list_block):
         #         cs += fn_block(c)
         #     c = cs / self.num_proc_blocks
-        c = self.fn["output_conv"](c)
+        c = self.fn["output_conv"](c.half()).float()
 
         return c #, e_
 
@@ -543,6 +543,9 @@ class SiFiGANGenerator(nn.Module):
 
         self.fn["blocks"] = filter_network
         del self.fn["upsamples"]
+
+        # try half precision
+        self.fn["output_conv"].half()
 
 
     def remove_weight_norm(self):
